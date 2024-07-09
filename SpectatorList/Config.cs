66dc2ee4e9d132253bf481a7c5ef37f3172d332f
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-07-09 11:59:44 +0200

    Descriptions for Config.cs

--- SpectatorList/Config.cs
+++ SpectatorList/Config.cs
@@ -17,11 +17,15 @@ namespace SpectatorList
 
         [Description("How often in seconds to refresh the hud")]
         public float RefreshRate { get; set; } = 1;
-
+        [Description("Location and size of the HUD")]
         public string FullText { get; set; } = "<size=20><align=right>%display%</size><voffset=900> </voffset></align>";
+        [Description("How the name of the player is displayed")]
         public string PlayerDisplay { get; set; } = "%name%";
+        [Description("What to show if no one is spectating, leave empty to remove")]
         public string NoSpectators { get; set; } = "No one is currently spectating you.";
+        [Description("The HUD when a player is spectating")]
         public string Spectators { get; set; } = "<color=#ff8c00>»👥 No Lifes«</color>";
+        [Description("Just a special name input, works only for a single steamID rn")]
         public string LunaNameColor { get; set; } = "🐻💜 <color=#FF10F0>%name%</color>";
     }
 }
