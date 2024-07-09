cfa9428f5ea216cabc8bf703bb3d5fdce0cc79d5
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-07-09 08:10:52 +0200

    Just my raw, unoptimized changes to this

--- SpectatorList/Config.cs
+++ SpectatorList/Config.cs
@@ -12,13 +12,16 @@ namespace SpectatorList
 
         [Description("A list of teams the hints should be hidden for")]
         public List<Team> HiddenFor { get; set; } = new List<Team>();
+        [Description("The Group Names of your Staff")]
+        public List<string> StaffRoleNames { get; set; } = new List<string>();
 
         [Description("How often in seconds to refresh the hud")]
-        public float RefreshRate { get; set; } = 2;
+        public float RefreshRate { get; set; } = 1;
 
-        public string FullText { get; set; } = "<size=23><align=right>%display%</size><voffset=900> </voffset></align>";
-        public string PlayerDisplay { get; set; } = "-> %name%";
+        public string FullText { get; set; } = "<size=20><align=right>%display%</size><voffset=900> </voffset></align>";
+        public string PlayerDisplay { get; set; } = "%name%";
         public string NoSpectators { get; set; } = "No one is currently spectating you.";
-        public string Spectators { get; set; } = "👥 Spectators (%amount%)";
+        public string Spectators { get; set; } = "<color=#ff8c00>»👥 No Lifes«</color>";
+        public string LunaNameColor { get; set; } = "🐻💜 <color=#FF10F0>%name%</color>";
     }
 }
--- SpectatorList/EventHandler.cs
+++ SpectatorList/EventHandler.cs
@@ -4,15 +4,14 @@ using System.Text;
 using Exiled.API.Features;
 using MEC;
 using PlayerRoles;
-using UnityEngine;
 
 namespace SpectatorList
 {
-    public class EventHandler
+public class EventHandler
     {
         private const string CoroutineTag = "Spectator-List"; 
 
-        private Config _config => SpectatorList.Instance.Config;
+        private static Config Config => SpectatorList.Instance.Config;
 
         public EventHandler()
         {
@@ -24,32 +23,73 @@ namespace SpectatorList
             Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
             Timing.KillCoroutines(CoroutineTag);
         }
-
+        
         private void OnRoundStarted() => Timing.RunCoroutine(DoList().CancelWith(Server.Host.GameObject), CoroutineTag);
 
         private IEnumerator<float> DoList()
         {
             while (true)
             {
-                foreach (Player player in Player.List)
+                foreach (var player in Player.List)
                 {
-                    if (player.IsDead || _config.HiddenFor.Contains(player.Role.Team)) continue;
+                    if (player.IsDead || Config.HiddenFor.Contains(player.Role.Team)) continue;
 
-                    int count = player.CurrentSpectatingPlayers.Count(p => p.Role != RoleTypeId.Overwatch);
+                    var count = player.CurrentSpectatingPlayers.Count(p => p.Role != RoleTypeId.Overwatch);
 
-                    StringBuilder sb = new StringBuilder();
+                    var sb = new StringBuilder();
                     sb.AppendLine(count == 0
-                        ? _config.NoSpectators
-                        : _config.Spectators.Replace("%amount%", count.ToString()));
+                        ? Config.NoSpectators
+                        : Config.Spectators.Replace("%amount%", count.ToString()));
 
-                    foreach (Player spectator in player.CurrentSpectatingPlayers.Where(p => p.Role != RoleTypeId.Overwatch))
-                        sb.AppendLine(_config.PlayerDisplay.Replace("%name%", spectator.CustomName));
-
-                    player.ShowHint(_config.FullText.Replace("%display%", sb.ToString()), _config.RefreshRate + 0.15f);
+                    var staff = new List<string>();
+                    staff.Add("0owner");
+                    staff.Add("0admin");
+                    staff.Add("0teamleitung");
+                    staff.Add("0communitymanager");
+                    staff.Add("0moderator");
+                    staff.Add("0senior_supporter");
+                    staff.Add("0supporter");
+                    staff.Add("0junior_supporter");
+                    
+                    foreach (var spectator in player.CurrentSpectatingPlayers.Where(p => p.Role != RoleTypeId.Overwatch))
+                    {
+                        var Role = "";
+                        if (spectator.GroupName.Contains("owner"))
+                            Role = "<color=red>" + "⌠Owner⌡</color> - "+ spectator.CustomName;
+                        if (spectator.GroupName.Contains("admin")) 
+                            Role = "<color=red>" + "⌠Admin⌡</color> - "+ spectator.CustomName;
+                        if (spectator.GroupName.Contains("teamleitung")) 
+                            Role = "<color=pumpkin>" + "⌠Teamleitung⌡</color> - "+ spectator.CustomName;
+                        if (spectator.GroupName.Contains("communitymanager")) 
+                            Role = "<color=yellow>" + "⌠Communitymanager⌡</color> - "+ spectator.CustomName;
+                        if (spectator.GroupName.Contains("moderator")) 
+                            Role = "<color=cyan>" + "⌠Moderator⌡</color> - "+ spectator.CustomName;
+                        if (spectator.GroupName.Contains("senior_supporter")) 
+                            Role = "<color=green>" + "⌠Senior Supporter⌡</color> - "+ spectator.CustomName;
+                        if (spectator.GroupName.Contains("supporter")) 
+                            Role = "<color=green>" + "⌠Supporter⌡</color> - "+ spectator.CustomName;
+                        if (spectator.GroupName.Contains("junior_supporter")) 
+                            Role = "<color=green>" + "⌠Junior Supporter⌡</color> - "+ spectator.CustomName;
+                        
+                        if (staff.Contains("0"+spectator.GroupName.Split(".")[0].Replace("0", "")))
+                        {
+                            sb.AppendLine(Config.PlayerDisplay.Replace("%name%", Role));
+                        }
+                        
+                        if (spectator.UserId == "76561199146721347@steam")
+                        {
+                            sb.AppendLine(Config.LunaNameColor.Replace("%name%", spectator.CustomName));
+                        }
+                        else if (spectator.UserId != "76561199146721347@steam" && !staff.Contains("0"+spectator.GroupName.Split(".")[0].Replace("0", "")))
+                        {
+                            sb.AppendLine(Config.PlayerDisplay.Replace("%name%", spectator.CustomName));
+                        }
+                    }
+                    player.ShowHint(Config.FullText.Replace("%display%", sb.ToString()), Config.RefreshRate + 0.15f);
                 }
 
-                yield return Timing.WaitForSeconds(_config.RefreshRate);
+                yield return Timing.WaitForSeconds(Config.RefreshRate);
             }
-        }
+        } 
     }
 }
